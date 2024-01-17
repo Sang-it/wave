e0a16feb712aa394fdf57eb9c29bac0af7fb1ce5
Sangit Manandhar <rux12@icloud.com>
2024-01-17 18:29:14 -0500

    chore : refactor example for parser

--- crates/wave_parser/examples/parser.rs
+++ crates/wave_parser/examples/parser.rs
@@ -1,19 +1,21 @@
-use std::{fs::File, io::Write, path::Path};
+use std::{env, fs::File, io::Write};
 
 use wave_allocator::Allocator;
 use wave_parser::Parser;
 
 fn main() {
-    let name = "test.wv".to_string();
-    let path = Path::new(&name);
-    let source_text = std::fs::read_to_string(path).unwrap_or_else(|_| panic!("{name} not found"));
+    let path = env::current_dir()
+        .expect("failed to get current directory")
+        .join("wave_parser/examples/source.wv");
+
+    let source_text = std::fs::read_to_string(path).unwrap_or_else(|_| panic!("file not found"));
     let allocator = Allocator::default();
     let ret = Parser::new(&allocator, &source_text).parse();
 
     let parsed = serde_json::to_string_pretty(&ret.program).unwrap();
 
     if ret.errors.is_empty() {
-        let mut f = File::create("foo.txt").unwrap();
+        let mut f = File::create("wave_parser/examples/source.txt").unwrap();
         f.write_all(parsed.as_bytes()).unwrap();
         println!("Parsed Successfully.");
     } else {
