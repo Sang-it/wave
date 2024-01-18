c4546cb656ae16ad8a0d3d0b2f968e4dfbd64abf
Sangit Manandhar <rux12@icloud.com>
2024-01-18 14:12:06 -0500

    feat : implement number literal , string literal and array expression

--- crates/wave_interpreter/src/eval/mod.rs
+++ crates/wave_interpreter/src/eval/mod.rs
@@ -1,20 +1,23 @@
 use wave_allocator::{Allocator, Box, Vec};
 use wave_ast::{
     ast::{
-        BindingPatternKind, Declaration, Expression, ExpressionStatement, IdentifierReference,
-        Program, Statement, VariableDeclaration, VariableDeclarator,
+        ArrayExpression, ArrayExpressionElement, BindingPatternKind, Declaration, Expression,
+        ExpressionStatement, IdentifierReference, Program, Statement, VariableDeclaration,
+        VariableDeclarator,
     },
-    BooleanLiteral,
+    BooleanLiteral, NumberLiteral, StringLiteral,
 };
 use wave_diagnostics::Result;
 
 use crate::{environment::Environment, Runtime};
+use std::vec::Vec as SVec;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum ER {
     Number(f64),
     Boolean(bool),
     String(String),
+    Array(SVec<ER>),
     Null,
 }
 
@@ -25,33 +28,25 @@ pub fn load_environment(environment: &mut Box<'_, Environment>) {
 pub fn eval_runtime(runtime: Runtime) -> Result<ER> {
     let mut environment = Box(runtime.arena.alloc(Environment::default()));
     load_environment(&mut environment);
-    eval_program(&runtime.program, &mut environment, runtime.arena)
+    eval_program(&runtime.program, &mut environment)
 }
 
-pub fn eval_program(
-    program: &Program<'_>,
-    environment: &mut Box<'_, Environment>,
-    arena: &Allocator,
-) -> Result<ER> {
+pub fn eval_program(program: &Program<'_>, environment: &mut Box<'_, Environment>) -> Result<ER> {
     let mut result = ER::Null;
     for statement in &program.body {
-        result = eval_statement(statement, environment, arena)?;
+        result = eval_statement(statement, environment)?;
     }
     Ok(result)
 }
 
-fn eval_statement(
-    statement: &Statement<'_>,
-    environment: &mut Box<'_, Environment>,
-    arena: &Allocator,
-) -> Result<ER> {
+fn eval_statement(statement: &Statement<'_>, environment: &mut Box<'_, Environment>) -> Result<ER> {
     match statement {
         Statement::ExpressionStatement(expression_stmt) => {
-            let result = eval_expression_statement(expression_stmt, environment, arena)?;
+            let result = eval_expression_statement(expression_stmt, environment)?;
             Ok(result)
         }
         Statement::Declaration(declaration) => {
-            let result = eval_declaration(declaration, environment, arena)?;
+            let result = eval_declaration(declaration, environment)?;
             Ok(result)
         }
         _ => unimplemented!(),
@@ -61,11 +56,10 @@ fn eval_statement(
 fn eval_declaration(
     declaration: &Declaration<'_>,
     environment: &mut Box<'_, Environment>,
-    arena: &Allocator,
 ) -> Result<ER> {
     match declaration {
         Declaration::VariableDeclaration(declaration) => {
-            let result = eval_variable_declaration(declaration, environment, arena)?;
+            let result = eval_variable_declaration(declaration, environment)?;
             Ok(result)
         }
         _ => unimplemented!("declaration"),
@@ -75,16 +69,14 @@ fn eval_declaration(
 fn eval_variable_declaration(
     declaration: &Box<'_, VariableDeclaration>,
     environment: &mut Box<'_, Environment>,
-    arena: &Allocator,
 ) -> Result<ER> {
-    eval_variable_declarator(&declaration.declarations, environment, arena)?;
+    eval_variable_declarator(&declaration.declarations, environment)?;
     Ok(ER::Null)
 }
 
 fn eval_variable_declarator(
     declarators: &Vec<'_, VariableDeclarator>,
     environment: &mut Box<'_, Environment>,
-    arena: &Allocator,
 ) -> Result<ER> {
     for declarator in declarators {
         if declarator.init.is_none() {
@@ -92,7 +84,7 @@ fn eval_variable_declarator(
         }
         match &declarator.id.kind {
             BindingPatternKind::BindingIdentifier(identifier) => {
-                let value = eval_expression(declarator.init.as_ref().unwrap(), environment, arena)?;
+                let value = eval_expression(declarator.init.as_ref().unwrap(), environment)?;
                 environment.define(identifier.name.to_owned(), value);
             }
         }
@@ -103,25 +95,34 @@ fn eval_variable_declarator(
 fn eval_expression_statement(
     expression_stmt: &Box<'_, ExpressionStatement>,
     environment: &mut Box<'_, Environment>,
-    arena: &Allocator,
 ) -> Result<ER> {
-    eval_expression(&expression_stmt.expression, environment, arena)
+    eval_expression(&expression_stmt.expression, environment)
 }
 
 fn eval_expression(
     expression: &Expression<'_>,
     environment: &mut Box<'_, Environment>,
-    _arena: &Allocator,
 ) -> Result<ER> {
     match expression {
         Expression::BooleanLiteral(expression) => eval_boolean_literal(expression),
+        Expression::NumberLiteral(expression) => eval_number_literal(expression),
+        Expression::StringLiteral(expression) => eval_string_literal(expression),
         Expression::Identifier(expression) => eval_identifier(expression, environment),
+        Expression::ArrayExpression(expression) => eval_array_expression(expression, environment),
         _ => unimplemented!(),
     }
 }
 
 fn eval_boolean_literal(expression: &Box<'_, BooleanLiteral>) -> Result<ER> {
     Ok(ER::Boolean(expression.value))
+}
+
+fn eval_number_literal(expression: &Box<'_, NumberLiteral>) -> Result<ER> {
+    Ok(ER::Number(expression.value))
+}
+
+fn eval_string_literal(expression: &Box<'_, StringLiteral>) -> Result<ER> {
+    Ok(ER::String(expression.value.to_string()))
 }
 
 fn eval_identifier(
@@ -132,5 +133,26 @@ fn eval_identifier(
     match value {
         Some(value) => Ok(value.clone()),
         None => Ok(ER::Null),
+    }
+}
+
+fn eval_array_expression(
+    expression: &Box<'_, ArrayExpression>,
+    environment: &mut Box<'_, Environment>,
+) -> Result<ER> {
+    let mut result = SVec::new();
+    for element in &expression.elements {
+        let value = eval_array_expression_element(element, environment)?;
+        result.push(value);
+    }
+    Ok(ER::Array(result))
+}
+
+fn eval_array_expression_element(
+    expression: &ArrayExpressionElement<'_>,
+    environment: &mut Box<'_, Environment>,
+) -> Result<ER> {
+    match expression {
+        ArrayExpressionElement::Expression(expression) => eval_expression(expression, environment),
     }
 }
